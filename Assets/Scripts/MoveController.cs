3ffcc67f7ff7deddc4a81297914bda63aaf2ab65
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-31 14:59:07 +0900

    FadeOut、画面リセット、その他修正

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -7,17 +7,19 @@ public class MoveController : MonoBehaviour
 {
     private Rigidbody rb;
     private float moveSpeed = 5.0f;
+    private float punchPower = 5.0f;
     private float angleSpeed = 10.0f;
 
     private Transform tf;
 
+    bool isFirst = true;  //Punchの最初のimpulseを判定するフラグ
+
     void Start()
     {
-        // rb = gameObject.GetComponent<Rigidbody>();
+        rb = gameObject.GetComponent<Rigidbody>();
         tf = gameObject.GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         //右スティックの値を格納、右側に倒すとｘ軸１に近づき、左に倒すと-1に近ずく。上下の場合、ｙ軸方向。ｚの値はない。最大値は１で、最小値は−１である。
@@ -29,7 +31,6 @@ public class MoveController : MonoBehaviour
 
         tf.Translate(velocity);
 
-
         //左スティック操作で角度を変換
         Vector3 leftStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
         Vector3 angle = new Vector3(0, leftStick.x * angleSpeed, 0);
@@ -38,10 +39,24 @@ public class MoveController : MonoBehaviour
 
         tf.Rotate(angle / 2);
 
+    }
 
+    //怒らせて、殴られた後後方に吹き飛ぶ
+    public void PunchPower()
+    {
+        if(isFirst)
+        {
+            isFirst = false;
+            // Vector3 firstImpulse = new Vector3 (0, 0, -10.0f);
+            Vector3 firstImpulse = new Vector3 (0, 0, -25.0f);
+            rb.AddForce(firstImpulse, ForceMode.Impulse);
+        }
+    }
 
-
-
-
+    public void secondPunchPower()
+    {
+        // Vector3 distance = new Vector3(0, 0, -1.0f);
+        Vector3 distance = new Vector3(0, 0, -4.0f);
+        rb.velocity = distance * punchPower;
     }
 }
